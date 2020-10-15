0f83ddb912baf98e53b16beb930ac079db8b1184
shunyue1320 <1277952981@qq.com>
2020-10-15 21:03:18 +0800

    😋

--- app.js
+++ app.js
@@ -3,32 +3,63 @@ const util = require('util')
 const qs = require('querystring')
 const requestPromise = util.promisify(request)
 
-let params = {
-  'courseId': 7277, //课程id
-  'classId': 28249  //课类id
-}
+class ForgedAttend {
+  constructor(options) {
+    let { courseId, classId, token } = options
 
-let options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'token': '700c1b50062b65c323bc4d82a21e59b2',  //用户token
-    'terminaltype': 4                             //上课终端类型
+    this.params = { courseId, classId }
+    this.reqOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': token,
+        'terminaltype': 4  //上课终端类型
+      }
+    }
+
+    this.init()
+  }
+
+  init() {
+    let time = 0
+    setInterval(() => {
+      this.attendRedress()
+      if (time % 5 === 0) {
+        this.attendCollect()
+      }
+      time++
+    }, 1000 * 60)
+  }
+
+  attendRedress() {
+    const options = Object.assign({}, this.reqOptions)
+    options.url = 'https://open.shiguangkey.com/api/attend/attendRedress'
+    this.request(options)
+  }
+
+  attendCollect() {
+    const options = Object.assign({}, this.reqOptions)
+    options.url = `https://attend.shiguangkey.com/api/attend/collect?${qs.stringify(this.params)}`
+    this.request(options)
+  }
+
+  request(options) {
+    requestPromise(options).then(res => {
+      let result = JSON.parse(res.body)
+      if (result.msg == 'success') {
+        console.log("正在上课中....")
+      } else {
+        console.log("上课失败", result.msg)
+      }
+    }).catch(e => {
+      console.log("上课失败", e);
+    })
   }
 }
 
-let _attendRedressUrl = 'https://open.shiguangkey.com/api/attend/attendRedress'
-let _attendCollectUrl = `https://attend.shiguangkey.com/api/attend/collect?${qs.stringify(params)}`
 
-
-options.url = _attendCollectUrl
-requestPromise(options).then(res => {
-  let result = JSON.parse(res.body)
-  if (result.msg == 'success') {
-    console.log("正在上课中....")
-  } else {
-    console.log("上课失败", result.msg)
-  }
-}).catch(e => {
-  console.log("上课失败", e);
+new ForgedAttend({
+  'courseId': 7277,                           //课程id
+  'classId': 28249,                           //课类id
+  'token': '700c1b50062b65c323bc4d82a21e59b2' //用户token
 })
